06a73361b33ef0af0d198645b19497c36f2e1b8a
Zhigang <georgewzg95@gmail.com>
2021-10-26 14:28:59 -0500

    clean the datasets a little bit

--- raw_designs/vtr_designs/verilog/LU64PEEng.v
+++ raw_designs/vtr_designs/verilog/LU64PEEng.v
@@ -19,6 +19,57 @@
 `define mFIFOWIDTH 6'b011100
 `define aFIFOWIDTH 5'b01000
 
+
+`define SIMULATION_MEMORY
+`ifdef SIMULATION_MEMORY
+
+module dual_port_ram(
+clk,
+addr1,
+addr2,
+data1,
+data2,
+we1,
+we2,
+out1,
+out2
+);
+
+parameter DATA_WIDTH = 256;
+parameter ADDR_WIDTH = 10;
+input clk;
+input [ADDR_WIDTH-1:0] addr1;
+input [ADDR_WIDTH-1:0] addr2;
+input [DATA_WIDTH-1:0] data1;
+input [DATA_WIDTH-1:0] data2;
+input we1;
+input we2;
+output reg [DATA_WIDTH-1:0] out1;
+output reg [DATA_WIDTH-1:0] out2;
+
+reg [DATA_WIDTH-1:0] ram[ADDR_WIDTH-1:0];
+
+always @(posedge clk) begin
+  if (we1) begin
+    ram[addr1] <= data1;
+  end
+  else begin
+    out1 <= ram[addr1];
+  end
+end
+
+always @(posedge clk) begin
+  if (we2) begin
+    ram [addr2] <= data2;
+  end
+  else begin
+    out2 <= ram[addr2];
+  end
+end
+
+endmodule
+`endif
+
 module LU64PEEng (clk, //ref_clk, global_reset_n,
  start, N, offset, done,
 		//mem_addr, mem_ba, mem_cas_n, mem_cke, mem_clk, mem_clk_n, mem_cs_n,
