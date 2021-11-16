a95030598849c1fc774e7111c04154aac8401ce6
Zhigang Wei <43479107+georgewzg95@users.noreply.github.com>
2021-11-15 18:52:06 -0600

    Update gen_submod.py

--- gen_submod.py
+++ gen_submod.py
@@ -46,7 +46,7 @@ class gen_submod():
   def write_submod(self):
     for i in range(len(self.dict) - 1, 0):
       for t_mod in self.dict[i]:
-        print("The curent level is " + str(i))
+        print(self.dict[i])
         self.generate(t_mod)
         self.append_mod(t_mod)
 
