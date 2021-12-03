b4f7986117478f2f0e8b8662502f6daed379f6cd
Zhigang Wei <43479107+georgewzg95@users.noreply.github.com>
2021-12-02 22:20:46 -0600

    Update run_cmds.py

--- run_cmds.py
+++ run_cmds.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import os
+import argparse
 
 report_dir = '/misc/scratch/zwei1/reports/'
 root_dir = '/misc/scratch/zwei1/raw_data'
@@ -53,56 +54,74 @@ def remove_design(design):
   with open(report_dir + os.sep + 'complete_jobs.txt', 'a+') as f:
     f.write(design.r_dir + ',' + design.filepath)
 
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-ct',
+                      '--create_directory',
+                      dest = 'create_directory',
+                      action = 'store_true')
+  parser.set_defaults(create_directory=False)
+  parser.add_argument('-rc',
+                      '--run_cmds',
+                      dest = 'run_cmds',
+                      action = 'store_true')
+  parser.set_defaults(run_cmds = False)
+  return args
+
 if __name__ == "__main__":
-  # file_list = open('out_data.csv', 'rt')
-  # list_designs = []
-  # for line in file_list:
-  #   verilog_filepath = line.split(',')[1][:-4]
-  #   design = Design(report_dir + verilog_filepath[:-2], root_dir + os.sep + verilog_filepath)
-  #   list_designs.append(design)
-  
-  # remain_jobs = open(report_dir + os.sep + 'remain_jobs.txt', 'w')
-  # for design in list_designs:
-  #   remain_jobs.write(design.r_dir + ',' + design.filepath + '\n')
 
-  # remain_jobs.close()
+  args = parse_args()
+  if args.create_directory == True:
+    file_list = open('out_data.csv', 'rt')
+    list_designs = []
+    for line in file_list:
+      verilog_filepath = line.split(',')[1][:-4]
+      design = Design(report_dir + verilog_filepath[:-2], root_dir + os.sep + verilog_filepath)
+      list_designs.append(design)
+    
+    remain_jobs = open(report_dir + os.sep + 'remain_jobs.txt', 'w')
+    for design in list_designs:
+      remain_jobs.write(design.r_dir + ',' + design.filepath + '\n')
 
-  remain_jobs_f = open(report_dir + os.sep + 'remain_jobs.txt', 'rt')
-  for line in remain_jobs_f:
-    design = Design(line.split(',')[0], line.split(',')[1])
-    remain_designs.append(design)
-  remain_jobs_f.close() 
+    remain_jobs.close()
 
-  num_jobs = 5
-  running_jobs = []
-  launched = False
+  if args.run_cmds == True:
+    remain_jobs_f = open(report_dir + os.sep + 'remain_jobs.txt', 'rt')
+    for line in remain_jobs_f:
+      design = Design(line.split(',')[0], line.split(',')[1])
+      remain_designs.append(design)
+    remain_jobs_f.close() 
 
-  while True:
-    if len(running_jobs) == 0 and launched == True:
-      break
+    num_jobs = 5
+    running_jobs = []
+    launched = False
 
-    for task in running_jobs:
-      if task.subproc.poll() is not None:
+    while True:
+      if len(running_jobs) == 0 and launched == True:
+        break
 
-        design = task.design
-        #print('error poll: ' + design.r_dir)
-        #remove design from the remain_jobs.txt and add it to complete_jobs.txt
-        remove_design(design)
+      for task in running_jobs:
+        if task.subproc.poll() is not None:
 
-        running_jobs.remove(task)
-        task.log.close()
-        task.err.close()
+          design = task.design
+          #print('error poll: ' + design.r_dir)
+          #remove design from the remain_jobs.txt and add it to complete_jobs.txt
+          remove_design(design)
 
-    while len(running_jobs) < num_jobs and len(remain_designs) > 0:
-      design = remain_designs[0]
-      remain_designs.remove(design)
-      log = open(design.r_dir + os.sep + 'log', 'w')
-      err = open(design.r_dir + os.sep + 'err', 'w')
-      replace_tcl(design)
-      subproc = subprocess.Popen([cmd], stdout=log, stderr=err, shell=True)
-      launched = True
-      task = Task(design, subproc, log, err)
-      running_jobs.append(task)
+          running_jobs.remove(task)
+          task.log.close()
+          task.err.close()
+
+      while len(running_jobs) < num_jobs and len(remain_designs) > 0:
+        design = remain_designs[0]
+        remain_designs.remove(design)
+        log = open(design.r_dir + os.sep + 'log', 'w')
+        err = open(design.r_dir + os.sep + 'err', 'w')
+        replace_tcl(design)
+        subproc = subprocess.Popen([cmd], stdout=log, stderr=err, shell=True)
+        launched = True
+        task = Task(design, subproc, log, err)
+        running_jobs.append(task)
 
 #cmd = 'vivado -mode batch -source tcl_scripts.tcl'
 #log = open('log', 'wt')
