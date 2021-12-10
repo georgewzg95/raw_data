c0b74922d5620d753d2d4ae61031247906266754
Zhigang Wei <43479107+georgewzg95@users.noreply.github.com>
2021-12-09 23:14:10 -0600

    added lasso

---
+++ model/lasso.py
@@ -0,0 +1,90 @@
+# import pandas as pd
+# from numpy import arange
+# from sklearn.linear_model import LassoCV
+# from sklearn.model_selection import RepeatedKFold
+
+# #specify URL where data is located
+# url = "https://raw.githubusercontent.com/Statology/Python-Guides/main/mtcars.csv"
+
+# #read in data
+# data_full = pd.read_csv(url)
+
+# #select subset of data
+# data = data_full[["mpg", "wt", "drat", "qsec", "hp"]]
+
+# #view first six rows of data
+# data[0:6]
+
+# #define predictor and response variables
+# X = data[["mpg", "wt", "drat", "qsec"]]
+
+# y = data["hp"]
+
+# #define cross-validation method to evaluate model
+# cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+# #define model
+# model = LassoCV(alphas=arange(0, 1, 0.01), cv=cv, n_jobs=-1)
+
+# #fit model
+# model.fit(X, y)
+
+# #display lambda that produced the lowest test MSE
+# print(model.alpha_)
+
+# #define new observation
+# new = [24, 2.5, 3.5, 18.5]
+
+# #predict hp value using lasso regression model
+# model.predict([new])
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import Lasso
+import os
+import argparse
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i_f',
+                     '--input_feature',
+                     required = True,
+                     type = str,
+                     help = 'the .feat input file')
+  parser.add_argument('-i_r',
+                      '--input_rpt',
+                      required = True,
+                      type = str,
+                      help = 'the .rpt input file')
+  args = parser.parse_args()
+  return args
+
+def retrieve_feature(file):
+  data_list = []
+  with open(file, "r") as f:
+  	lines = f.readlines()
+  for line in lines:
+  	temp = line.split(',')[1:]
+  	temp = [float(i) for i in temp]
+  	data_list.append(temp)
+  return np_array(data_list)
+
+def retrieve_report(file):
+  data_list = []
+  with open(file, "r") as f:
+    lines = f.readlines()
+
+  for line in lines:
+    data_list.append(float(line.split(',')[-1]))
+  return np.array(data_list)
+
+if __name__ == "__main__":
+  args = parse_args()
+  feature_file = args.input_feature
+  report_file = args.input_rpt
+  X = retrieve_feature(feature_file)
+  y = retrieve_report(report_file)
+  print(X.shape)
+  print(y.shape)
